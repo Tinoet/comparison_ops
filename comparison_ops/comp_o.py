6df284c786b4098853678aa55f8e94e9b4d6f99e
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-21 23:25:41 +0100

    Add files via upload

---
+++ comparison_ops/comp_o.py
@@ -0,0 +1,8 @@
+# >,==,>=,<=
+name = "Elijah  Tochi"
+if len(name) < 3:
+  print( "name must be a maximum of 3 characters")
+elif len(name) > 50:
+  print("name must be a minimum of 50 characters")
+else:
+  print("name looks good")
